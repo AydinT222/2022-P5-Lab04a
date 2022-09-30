d4114ebc24c198418ec0fdef401f0729ccd1fa9a
Aydin <70951773+AydinT222@users.noreply.github.com>
2022-09-30 13:11:42 -0400

    definitely a 100 point version

--- Lab04avst.java
+++ Lab04avst.java
@@ -7,19 +7,32 @@ public class Lab04avst
 {
     public static void main(String[] args)
     {
-        System.out.println("Lab04a, Student Version\n");
+        System.out.println("Lab04a, 100 Point Version\n");
 
-        double principal  = 250000;
-        double annualRate = 4.85;
-        double numYears   = 30;
+        double principal    = 250000;
+        double annualRate   = 4.85;
+        double numYears     = 30;
 
+        // below is only for monthlyPay
+        double monthlyRate  = (annualRate / 100) / 12; // 0.004...
+        double numMonths    = numYears * 12; // 360.0
+        // top is only for monthlyPay
 
+        double numerator = (monthlyRate * Math.pow((1 + monthlyRate), numMonths));
+        double denominator = (Math.pow((1 + monthlyRate), numMonths) - 1);
 
+        double monthlyPay = (numerator / denominator) * principal;
 
+        double totalPayment = monthlyPay * numMonths;
+        double totalInterest = totalPayment - principal;
 
-
-
-
+        // the casting part looks terrible
+        System.out.println("Principal:\t\t\t$" + principal);
+        System.out.println("Annual Rate:\t\t" + annualRate + "%");
+        System.out.println("Number of Years:\t" + numYears);
+        System.out.println("Monthly Payment:\t" + (double)(Math.round(monthlyPay * 100)) / 100); // sorry
+        System.out.println("Total Payments:\t\t" + (double)(Math.round(totalPayment * 10)) / 10); //474922.8
+        System.out.println("Total Interest:\t\t" + (double)((int)(totalInterest * 10)) / 10); //224922.8
 
         System.out.println();
     }
